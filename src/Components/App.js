f99b336128eb91b85b5093591ff4e51b939498a8
shaheerkhalid <shaheerkhalid12@gmail.com>
2020-02-13 23:29:48 +0500

    file structure

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import { GREEN } from 'constants'
 import './App.css';
 
 function App() {
-  return (
-    <h1>MIR System</h1>
-  );
+  return (<h2>test</h2>);
 }
 
 export default App;
