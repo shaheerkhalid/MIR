51dff2e7b856f91f2a32144037800d9dc6561840
WajidAwais <wajidawais429@gmail.com>
2020-02-19 22:45:47 +0500

    half index

---
+++ src/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Components/Header';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+
+export default function Home() {
+    return(
+        <div>
+            <Navbar/>
+            <Header/>
+            <Footer/>
+        </div>    
+    );
+} 
