091c3a642cd455d43f91f01fdb5a267ca5e69b7b
shaheerkhalid <shaheerkhalid12@gmail.com>
2020-02-21 16:43:24 +0500

    merge fix

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -5,17 +5,13 @@ import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import AccountCircle from '@material-ui/icons/AccountCircle';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import {BLACK , WHITE , RED } from '../Constants';
-import Button from '@material-ui/core/Button';
+import {List,ListItem} from '@material-ui/core';
 
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-    },
-  },
-
   grow: {
     flexGrow: 1,
   },
@@ -29,12 +25,15 @@ const useStyles = makeStyles(theme => ({
       background: WHITE,
       color: BLACK,
   }
-
 }));
 
 export default function PrimarySearchAppBar() {
   const classes = useStyles();
-  
+  const flexContainer = {
+    display: 'flex',
+    flexDirection: 'row',
+    padding: 0,
+  };
   return (
     <div className={classes.grow}>
       <AppBar className={classes.navstyle} position="fixed">
@@ -42,18 +41,10 @@ export default function PrimarySearchAppBar() {
           <Typography className={classes.title} variant="h4" noWrap style={{color: RED , fontWeight: 'bold'}}>
             MIRS
           </Typography>
-          <div className={classes.root}>
-            <Button>Contact Us</Button>
-            <Button>About Us</Button>
-            
-          </div>
-
-
-
-          {/* <List style={flexContainer}>
+          <List style={flexContainer}>
             <ListItem>Contact</ListItem>
             <ListItem>About</ListItem>
-          </List> */}
+          </List>
             {/* <Tabs aria-label="simple tabs example">
                 <Tab label="Post Instrument"/>
                 <Tab label="Contact"/>
@@ -61,7 +52,6 @@ export default function PrimarySearchAppBar() {
             </Tabs>  */}
           <div className={classes.grow} />
           <div className={classes.sectionDesktop}>
-          <Button style={{alignItems: 'right', backgroundColor: RED, color: WHITE}}>Post Instrument</Button>
             <IconButton
               edge="end"
               aria-label="account of current user"
