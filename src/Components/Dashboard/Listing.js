8fccab3bacc610fa764a6605ca6672a7193091a3
shaheer <shaheerkhalid12@gmail.com>
2020-05-08 15:35:07 +0500

    user panel listing update

--- src/Components/Dashboard/Listing.js
+++ src/Components/Dashboard/Listing.js
@@ -8,33 +8,36 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
+import {useSelector} from 'react-redux';
+import bodyParser from 'body-parser';
 
 const columns = [
   { id: 'title', label: 'Title', minWidth: 150 },
-  { id: 'created', label: 'Created', minWidth: 150 },
-  { id: 'updated', label: 'Updated', minWidth: 150 },
-  { id: 'category', label: 'Category', minWidth: 150 },
+  { id: 'product_type', label: 'Type', minWidth: 150 },
+  { id: 'date_added', label: 'Date', minWidth: 150 },
+  { id: 'price_per_day', label: 'Price', minWidth: 150 },
   { id: 'status', label: 'Status', minWidth: 150 },
 ];
 
-function createData(title, created, updated, category) {
-  const status="true";
-  return { title, created, updated, category, status };
-}
 
-const rows = [
-  createData('Guitar Y30', '10/02/2020', '10/02/2020', 'Guitar'),
-  createData('Guitar Y30', '10/02/2020', '10/02/2020', 'Guitar'),
-  createData('Guitar Y30', '10/02/2020', '10/02/2020', 'Guitar'),
-  createData('Guitar Y30', '10/02/2020', '10/02/2020', 'Guitar'),
-  createData('Guitar Y30', '10/02/2020', '10/02/2020', 'Guitar'),
-  createData('Guitar Y30', '10/02/2020', '10/02/2020', 'Guitar'),
-  createData('Guitar Y30', '10/02/2020', '10/02/2020', 'Guitar'),
-  createData('Guitar Y30', '10/02/2020', '10/02/2020', 'Guitar'),
-  createData('Guitar Y30', '10/02/2020', '10/02/2020', 'Guitar'),
-  createData('Guitar Y30', '10/02/2020', '10/02/2020', 'Guitar'),
-  createData('Guitar Y30', '10/02/2020', '10/02/2020', 'Guitar'),
-];
+
+// function createData(title, type, data, price, status) {
+//   return { title, type, data, price, status };
+// }
+
+// const rows = [
+//   createData('Guitar Y30', '10/02/2020', '10/02/2020', 'Guitar'),
+//   createData('Guitar Y30', '10/02/2020', '10/02/2020', 'Guitar'),
+//   createData('Guitar Y30', '10/02/2020', '10/02/2020', 'Guitar'),
+//   createData('Guitar Y30', '10/02/2020', '10/02/2020', 'Guitar'),
+//   createData('Guitar Y30', '10/02/2020', '10/02/2020', 'Guitar'),
+//   createData('Guitar Y30', '10/02/2020', '10/02/2020', 'Guitar'),
+//   createData('Guitar Y30', '10/02/2020', '10/02/2020', 'Guitar'),
+//   createData('Guitar Y30', '10/02/2020', '10/02/2020', 'Guitar'),
+//   createData('Guitar Y30', '10/02/2020', '10/02/2020', 'Guitar'),
+//   createData('Guitar Y30', '10/02/2020', '10/02/2020', 'Guitar'),
+//   createData('Guitar Y30', '10/02/2020', '10/02/2020', 'Guitar'),
+// ];
 
 const useStyles = makeStyles({
   root: {
@@ -45,10 +48,17 @@ const useStyles = makeStyles({
   },
 });
 
+
+
 export default function Listing() {
   const classes = useStyles();
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(8);
+  const jsontoken = useSelector(state => state.jsontoken);
+  const user = useSelector(state => state.userid);
+  const [data, setdata] = React.useState(null);
+  let rows=null;
+  
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -59,7 +69,69 @@ export default function Listing() {
     setPage(0);
   };
 
+
+  React.useEffect(()=>{
+    fetch(`http://localhost:5000/Api/Product/ByUserID/${user.user_id}`,  {
+                  method: 'GET',
+                  headers: { 'Content-Type': 'application/json' ,
+                              'Authorization': jsontoken
+                          }
+                    
+                      })
+              .then(res => res.json())
+              .catch(error => console.error('Error:', error))
+              .then(response => {
+                  if(response.success===1){
+                    console.log(response);
+                    rows=response.data;
+                    console.log(rows);
+                    setdata(response.data);
+                  }
+              });
+  },[user.user_id])
+
+  function editHandler(productID){
+    console.log(productID);
+    
+  }
+
+  function statusHandler(productID){
+    fetch(`http://localhost:5000/Api/Product/ByUserID`,  {
+                  method: 'PATCH',
+                  headers: { 'Content-Type': 'application/json' ,
+                              'Authorization': jsontoken
+                          },
+                  body: JSON.stringify({"product_id":productID})
+                    
+                      })
+                
+              .then(res => res.json())
+              .catch(error => console.error('Error:', error))
+              .then(response => {
+                  if(response.success===1){
+                    fetch(`http://localhost:5000/Api/Product/ByUserID/${user.user_id}`,  {
+                      method: 'GET',
+                      headers: { 'Content-Type': 'application/json' ,
+                                  'Authorization': jsontoken
+                              }
+                        
+                          })
+                  .then(res => res.json())
+                  .catch(error => console.error('Error:', error))
+                  .then(response => {
+                      if(response.success===1){
+                        console.log(response.data);
+                        rows=response.data;
+                      }
+                  });     
+                  }
+              });
+
+    
+  } 
+
   return (
+    (rows !== null)?
     <div>
       <br></br>
     <Paper className={classes.root}>
@@ -76,6 +148,13 @@ export default function Listing() {
                   {column.label}
                 </TableCell>
               ))}
+                <TableCell
+                  key="action"
+                  // align="300"
+                  style={{ minWidth: 300 }}
+                >
+                  Actions
+                </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -90,6 +169,12 @@ export default function Listing() {
                       </TableCell>
                     );
                   })}
+                  <TableCell key="action" align="300">
+                        {/* {column.format && typeof value === 'number' ? column.format(value) : value} */}
+                        <button onClick={editHandler(row.product_id)}>Edit</button>
+                        <button onClick={statusHandler(row.product_id)}> Inactive</button>
+                        
+                      </TableCell>
                 </TableRow>
               );
             })}
@@ -106,6 +191,6 @@ export default function Listing() {
         onChangeRowsPerPage={handleChangeRowsPerPage}
       />
     </Paper>
-    </div>
+    </div>:<div></div>
   );
 }
