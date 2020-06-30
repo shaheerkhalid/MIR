6d49856b707f34e531771700407cc52ce60dc60c
WajidAwais <wajidawais429@gmail.com>
2020-06-30 11:35:05 +0500

    add ReturnReview File

---
+++ src/Components/Dashboard/ReturnReview.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+export default function ReturnReview() {
+  const [open, setOpen] = React.useState(false);
+  const [prodReview, setProdReview] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        Open form dialog
+      </Button>
+    </div>
+  );
+}
